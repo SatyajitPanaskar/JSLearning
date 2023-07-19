7cc60a21e8324270b5439dc43bb06212cbbfbf40
SatyajitPanaskar <satyajitpanaskarsp@gmail.com>
2023-07-19 10:44:08 +0530

    Basic variable JavaScript
    Basic variable JavaScript

---
+++ 01_variableAssign.js
@@ -0,0 +1,24 @@
+var myFullName;
+console.log("Type of Variable myFullName => ", myFullName);
+myFullName ="Satyajit Panaskar";
+console.log("First and Last Name => ", myFullName);
+myFullName = "Satyajit Ranjit Panaskar";
+console.log("My Full Name => ", myFullName);
+var myLove = "I Love Only JS";
+console.log(myLove);
+
+var firstName ="Satyajit";
+console.log("First Name =>",firstName);
+var lastName ="Panaskar";
+console.log("Last Name => ", lastName);
+var age = 23;
+console.log("my age is =>", age);
+var address ="Pimple Gurav, Pune";
+console.log("Address => ", address);
+var pinCode =432234;
+console.log("PinCode => ", pinCode);
+var gender ="male";
+console.log("Gender => ", gender);
+var isMarried ="Unmarried";
+console.log("Married Status => ",isMarried);
+
