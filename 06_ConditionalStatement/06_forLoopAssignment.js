b0796ffa1536dda163ca64e702bcdc121a13d86a
SatyajitPanaskar <satyajitpanaskarsp@gmail.com>
2023-07-31 10:35:48 +0530

    Using For and while loop
    Using For and while loop

---
+++ 06_ConditionalStatement/06_forLoopAssignment.js
@@ -0,0 +1,73 @@
+console.log(`-----------------Using For Loop Assignment 01------`);
+
+console.log(`------Step - 1 : Print numbers from 5 to 15 -------`);
+var result = "";
+for (let index = 5; index <=15; index++)
+{
+   
+   console.log(index); 
+}
+
+console.log(`------------------------------------------`);
+
+console.log(`-------Step - 2 : print number from 50 to 40 ---------`);
+
+for (let index = 50; index >40; index--)
+{
+    console.log(index);
+    
+}
+
+console.log(`------------------------------------------`);
+
+console.log(`-------Step - 3 :find first 15 odd numbers ---------`);
+console.log(`-----ODD Number-----`);
+for (let index = 1; index <= 30; index = index+2)
+{
+console.log(index);   
+    
+}
+
+
+console.log(`------------------------------------------`);
+
+console.log(`-------Step - 4 :find first 15 EVEN numbers ---------`);
+console.log(`-----EVEN Number-----`);
+for (let index = 0; index <= 30; index = index+2)
+{
+console.log(index);   
+    
+}
+
+console.log(`------------------------------------------`);
+
+console.log(`-------Step - 4 :find table of 5 ---------`);
+
+for (let index = 5; index <= 50; index = index+5)
+{
+console.log(index);   
+    
+}
+
+
+console.log(`------------------------------------------`);
+
+console.log(`-------Step - 4 :find table of 10 ---------`);
+
+for (let index = 10; index <= 100; index = index+10)
+{
+console.log(index);   
+    
+}
+
+
+console.log(`------------------------------------------`);
+
+console.log(`-------Step - 4 :find table of 10 reverse order ---------`);
+
+for (let index = 100; index >= 10; index = index-10)
+{
+console.log(index);   
+    
+}
+
---
+++ 06_ConditionalStatement/06_whileLoopAssignement.js
@@ -0,0 +1,63 @@
+console.log(`-----------------Using While Loop Assignment 02------`);
+
+console.log(`---Step -1 Print numbers from 5 to 15 -----`);
+var i = 5;
+while (i <= 15)
+{
+    console.log(i);
+    i++;
+}
+console.log(`-------------------------`);
+console.log(`---Step -2 Print numbers from 50 to 40 -----`);
+var i = 50;
+while (i >= 40)
+{
+    console.log(i);
+    i--;    
+}
+console.log(`-------------------------`);
+console.log(`---Step -3 find first 15 odd numbers -----`);
+var i = 1;
+while (i <= 30)
+{
+    console.log(i);
+    i=i+2;   
+}
+
+console.log(`-------------------------`);
+console.log(`---Step -4 find first 15 EVEN numbers -----`);
+var i = 0;
+while (i <= 30)
+{
+    console.log(i);
+    i=i+2;   
+}
+
+
+console.log(`-------------------------`);
+console.log(`---Step -5 Table of 5 -----`);
+var i = 5;
+while (i <= 50)
+{
+    console.log(i);
+    i=i+5;   
+}
+
+
+console.log(`-------------------------`);
+console.log(`---Step -6 Table of 10 -----`);
+var i = 10;
+while (i <= 100)
+{
+    console.log(i);
+    i=i+10;   
+}
+
+console.log(`-------------------------`);
+console.log(`---Step -7 Table of 10 -----`);
+var i = 100;
+while (i >= 10)
+{
+    console.log(i);
+    i=i-10;   
+}
